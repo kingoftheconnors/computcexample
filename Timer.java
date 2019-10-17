004ba9d0f46203017c03d2f9307d18f3d223cbf5
kingoftheconnors <Connormcp777@gmail.com>
2019-10-17 17:20:27 -0400

    Added wait command

--- Timer.java
+++ Timer.java
@@ -1,6 +1,9 @@
+import java.util.concurrent.TimeUnit;
 
 public class Timer {
     public static void main(String args[]) {
+        System.out.println("Hello World!");
+        TimeUnit.SECONDS.sleep(10);
         System.out.println("Hello World!");
     }
 }
